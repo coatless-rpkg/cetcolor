bbbb32dfa1d514312b702e9204ba93e8139f85ec
James Balamuta <balamut2@illinois.edu>
2020-01-06 12:16:26 -0600

    Update package level docs

--- R/cetcolor-package.R
+++ R/cetcolor-package.R
@@ -1,3 +1,10 @@
 #' @details
 #' 56 Colour maps are available as of May 2018
+#' @keywords internal
 "_PACKAGE"
+
+# The following block is used by usethis to automatically manage
+# roxygen namespace tags. Modify with care!
+## usethis namespace: start
+## usethis namespace: end
+NULL
